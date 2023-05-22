5a4642d6165bb1387ab9e65ee390e73b7653a6af
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-05-22 15:49:56 +0530

    Update

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,9 +4,9 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <ul className="sidebar-links">
-        <li><a href="#">Google Drive</a></li>
-        <li><a href="#">Google Sheets</a></li>
-        <li><a href="#">Google Slides</a></li>
+        <li><a href="https://drive.google.com/">Google Drive</a></li>
+        <li><a href="https://sheets.google.com/">Google Sheets</a></li>
+        <li><a href="https://slides.google.com/">Google Slides</a></li>
       </ul>
     </div>
   );
